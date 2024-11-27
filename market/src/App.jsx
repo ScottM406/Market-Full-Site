965c45ef1454497ab28219a880091726234af5b7
Scott <Scottyjmt13@gmail.com>
2024-11-27 12:02:54 -0700

    Add Login Page HTML. Styled.

--- market/src/App.jsx
+++ market/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./homepage";
+import Homepage from "./Homepage.jsx";
+import Login from "./Login.jsx";
 
 function App() {
 
 return (
   <>
     <Routes>
-      <Route path="/" element={<Homepage />}/>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element= {<Login />} />
     </Routes>
   </>
 )
