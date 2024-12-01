ba0200589b58e15937025b4fe9eae9bb4b3e2264
Scott <Scottyjmt13@gmail.com>
2024-11-30 20:03:32 -0700

    Add token props. Add logged-in Navbar format

--- market/src/App.jsx
+++ market/src/App.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Homepage from "./Homepage.jsx";
 import Login from "./Login.jsx";
 import Navbar from "./Navbar.jsx";
 import Register from "./Register.jsx";
 
-function App() {
+const App = () => {
+
+  const [loginToken, setLoginToken] = useState("");
 
 return (
   <>
-    <Navbar />
+    <Navbar loginToken={loginToken}/>
     <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/login" element= {<Login />} />
+      <Route path="/" element={<Homepage loginToken={loginToken} />} />
+      <Route path="/login" element= {<Login setLoginToken={setLoginToken} />} />
       <Route path="/register" element= {<Register />} />
     </Routes>
   </>
