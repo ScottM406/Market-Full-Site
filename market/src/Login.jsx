90f5d4886ddd92310d9d5d4930b4474ed4a87052
Scott <Scottyjmt13@gmail.com>
2024-11-30 18:50:11 -0700

    addded login fuctionality

--- market/src/Login.jsx
+++ market/src/Login.jsx
@@ -1,19 +1,45 @@
 import { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const submitHandler = async (event) => {
+    
+    event.preventDefault();
+    
+    try {
+
+      const postCredentials = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify({
+          username: username,
+          password: password
+        }),
+      });
+
+      if (!postCredentials.ok) {
+        throw new Error();
+      }
+
+      const responseJSON = await postCredentials.json();
+      console.log(responseJSON);
+
+    } catch (e) {
+      alert("Invalid Credentials");
+    }
+  }
 
   return (
     <>
-      <form id="login-form">
+      <form id="login-form" onSubmit={submitHandler}>
         <h1>Please log in below to shop online and view your existing orders</h1>
         <input
-          placeholder= "email"
-          type= "email"
-          value= {email}
+          placeholder= "username"
+          value= {username}
           required
-          onChange= {(event) => setEmail(event.target.value)}
+          onChange= {(event) => setUsername(event.target.value)}
         />
         <input
           placeholder= "password"
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(require("./api/auth").router);
 
